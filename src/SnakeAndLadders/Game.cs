9b717d3ba6da209594037a87b821183f55fc2802
Bala-Iyer <bala.iyer1973@gmail.com>
2019-03-04 23:31:20 +0000

    Fixed token position at start of game test

--- src/SnakeAndLadders/Game.cs
+++ src/SnakeAndLadders/Game.cs
@@ -3,5 +3,10 @@ namespace SnakeAndLadders
     public class Game
     {
         public int TokenPosition { get; }
+
+        public Game()
+        {
+            TokenPosition = 1;
+        }
     }
 }
