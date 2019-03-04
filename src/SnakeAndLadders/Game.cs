aa2497cd6af722fb8cd2f31b153109311a5853c4
Bala-Iyer <bala.iyer1973@gmail.com>
2019-03-04 23:50:57 +0000

    When token position is 100, game is won

--- src/SnakeAndLadders/Game.cs
+++ src/SnakeAndLadders/Game.cs
@@ -3,13 +3,22 @@ namespace SnakeAndLadders
     public class Game
     {
         public int TokenPosition => _token.Position;
-        public bool Won { get; }
 
         private readonly Token _token;
+        
+        private readonly int _winningPosition;
 
-        public Game()
+        public bool Won => TokenPosition == _winningPosition;
+
+        private Game(int winningPosition)
         {
+            _winningPosition = winningPosition;
             _token = new Token();
+        }
+
+        public Game() : this(100)
+        {
+            
         }
 
         public void MoveBy(int spaces)
