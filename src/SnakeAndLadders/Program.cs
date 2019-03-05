44083202f48be72aa872d39869af4a24749fa08a
Bala-Iyer <bala.iyer1973@gmail.com>
2019-03-05 00:14:20 +0000

    Console app now starts a game and moves token position till game is won

--- src/SnakeAndLadders/Program.cs
+++ src/SnakeAndLadders/Program.cs
@@ -6,7 +6,17 @@ namespace SnakeAndLadders
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var game = new Game();
+            var dice = new Dice();
+
+            Console.WriteLine($"Game started, Current position {game.TokenPosition}");
+            while (!game.Won)
+            {
+                var roll = dice.Roll();
+                game.MoveBy(roll);
+                Console.WriteLine($"Dice roll: {roll}, Current Position: {game.TokenPosition}");
+            }
+            Console.WriteLine("Game won !!!");        
         }
     }
 }
